72cbaebb45f3cf6d3b76e509cfc51acbf051100d
emontalban <mentulbenodarna@gmail.com>
2026-05-10 10:03:14 +0200

    añadido el ejercicio 14

---
+++ 14.capitalize.js
@@ -0,0 +1,10 @@
+// Poner en mayusculas todas la palabras
+
+const toCapital = str =>{
+    const words= str.split(" ");
+   return words.map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+
+    }
+
+const longStr = "the quick brown fox jumped over the lazy dog";
+toCapital(longStr); // The Quick Brown Fox Jumped Over The Lazy Dog
